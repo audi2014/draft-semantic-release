a2a6526ccafd16ed94d3b319e8ba43fa371ecbd8
Artur <artur.perepelytsia@hive.com>
2026-01-12 13:25:51 +0200

    no message

---
+++ release.config.js
@@ -0,0 +1,60 @@
+const branch = process.env.CI_COMMIT_BRANCH || 'master';
+const isReleaseBranch = ['master'].includes(branch);
+
+const branches = [
+  "master",
+  {
+    "name": "beta",
+    "prerelease": true,
+  },
+  {
+    "name": "devel",
+    "prerelease": true,
+  },
+];
+
+const pluginCommitAnalyzerConfig = [
+  "@semantic-release/commit-analyzer",
+  {
+    "preset": "angular",
+    "releaseRules": [
+      {
+        "type": "fix",
+        "release": "patch",
+      },
+      {
+        "type": "feat",
+        "release": "minor",
+      },
+      {
+        "type": "refactor",
+        "release": "patch",
+      },
+      {
+        "type": "perf",
+        "release": "patch",
+      },
+      {
+        "type": "chore",
+        "release": "patch",
+      },
+    ],
+  },
+]
+
+const plugins = [
+  pluginCommitAnalyzerConfig,
+  ...(isReleaseBranch ? [
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+  ] : []),
+  "@semantic-release/npm",
+  "@semantic-release/git",
+]
+
+const config = {
+  branches,
+  plugins,
+};
+
+module.exports = config;
